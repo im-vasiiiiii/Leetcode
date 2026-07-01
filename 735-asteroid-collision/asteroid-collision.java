61abc96e3a39be8cb8432383ac13ec4626f344f0
im-vasiiiiii <muhmmadvasi02@gmail.com>
2026-07-01 17:33:11 +0530

    Time: 7 ms (11.79%) | Memory: 47.1 MB (30.06%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean alive = true;
+
+            while (alive && a < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                int top = stack.peek();
+                if (top < -a) {
+                    stack.pop(); // top explodes, continue checking
+                } else if (top == -a) {
+                    stack.pop(); // both explode
+                    alive = false;
+                } else {
+                    alive = false; // current asteroid explodes
+                }
+            }
+
+            if (alive) {
+                stack.push(a);
+            }
+        }
+
+        // Convert stack to array
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
