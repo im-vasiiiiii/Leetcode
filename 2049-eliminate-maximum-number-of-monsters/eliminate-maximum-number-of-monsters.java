cdd788c74db706de22ff505e1d8746c6f7cc263f
im-vasiiiiii <muhmmadvasi02@gmail.com>
2026-07-06 14:59:22 +0530

    Time: 20 ms (87.61%) | Memory: 79.6 MB (60.55%) - LeetSync

---
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+
+        int n = dist.length;
+        int[] time = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            time[i] = (dist[i] - 1) / speed[i];
+        }
+
+        Arrays.sort(time);
+
+        for (int minute = 0; minute < n; minute++) {
+            if (time[minute] < minute) {
+                return minute;
+            }
+        }
+
+        return n;
+    }
+}
