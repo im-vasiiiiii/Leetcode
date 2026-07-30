041c69abc6d9a4ed6f3f359ce541a0413781c6fb
im-vasiiiiii <muhmmadvasi02@gmail.com>
2026-07-30 18:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.25%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    static int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int current = first + second;
+            first = second;
+            second = current;
+        }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climbStairs(n));
+    }
+}
